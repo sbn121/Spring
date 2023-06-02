4df58e5cc9ee948b9aafeefda7215e1f799ff1eb
sbn121 <129023074+sbn121@users.noreply.github.com>
2023-06-02 14:11:03 +0900

    2023.06.02

--- Workspace/02.smart/src/main/java/customer/CustomerDAO.java
+++ Workspace/02.smart/src/main/java/customer/CustomerDAO.java
@@ -3,20 +3,21 @@ package customer;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CustomerDAO implements CustomerService{
-	private SqlSession sql;
+	@Autowired private SqlSession sql;
 	
 	// 생성된 객체(빈으로 등록된 객체들)의 주소를 스프링 container에 관리가 됨
 	// IoC(Inversion of Control) 개발자가 필요할 때마다 new로 객체를 생성 -> 스프링프레임웤이 객체를 생성
 	
 	// DI(Dependency Injection) 객체의 주소를 담아주는(주입) 처리
 	// 필드에 데이터 담는 방법 2가지
-	public CustomerDAO(SqlSession sql) {
-		this.sql = sql;
-	}
+//	public CustomerDAO(SqlSession sql) {
+//		this.sql = sql;
+//	}
 	
 	@Override
 	public void customer_insert(CustomerVO vo) {
@@ -26,8 +27,7 @@ public class CustomerDAO implements CustomerService{
 
 	@Override
 	public List<CustomerVO> customer_list() {
-		// TODO Auto-generated method stub
-		return null;
+		return sql.selectList("customer.list");
 	}
 
 	@Override
--- Workspace/02.smart/src/main/webapp/WEB-INF/views/customer/list.jsp
+++ Workspace/02.smart/src/main/webapp/WEB-INF/views/customer/list.jsp
@@ -9,16 +9,40 @@
 <body>
 	<h3 class="my-4">고객목록</h3>
 	<table class="tb-list">
+		<colgroup>
+			<col width="200px">
+			<col width="300px">
+			<col width="400px">
+		</colgroup>
 		<tr>
 			<th>고객명</th>
 			<th>전화번호</th>
 			<th>이메일</th>
 		</tr>
+		<%-- 
+		<%
+		for(int i=1;i<=10;i++) {
+			System.out.print(i);
+		}
+		String[] names = {"홍길동", "박문수", "심청"};
+		for(int idx=0; idx<names.length; idx++) {
+			System.out.print(names[idx]);
+		}
+		for(String name : names) {
+			System.out.print(name);
+		}
+		%>
+		--%>
+		<c:forEach items="${list }" var="vo" >
 		<tr>
-			<td>홍길동</td>
-			<td>010.1234.5678</td>
-			<td>hgd@naver.com</td>
+			<td>${vo.name }</td>
+			<td>${vo.phone }</td>
+			<td>${vo.email }</td>
 		</tr>
+		</c:forEach>
+		<c:if test="${empty list }">
+		<tr><td colspan='3'>고객목록이 없습니다.</td></tr>
+		</c:if>
 	</table>
 </body>
 </html>
